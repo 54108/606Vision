8f527713619775da6b741d2723e132fa919a2843
54108 <3318195572@qq.com>
2024-03-14 18:06:26 +0800

    add Camera

--- main.cpp
+++ main.cpp
@@ -8,83 +8,19 @@
  * @copyright Copyright (c) 2024
  *
  */
-#define USING_GUI
 #define DEBUG
 #ifdef DEBUG
 #define DBG std::cout << __FILE__ << ":" << __LINE__ << ":" << __TIME__ << std::endl;
 #endif
 
 #include "Camera/MVCamera.hpp"
-#include "Imgui/imgui.h"
-#include "Imgui/imgui_impl_glfw.h"
-#include "Imgui/imgui_impl_opengl3.h"
-#include <GLFW/glfw3.h> // Will drag system OpenGL headers
 #include <iostream>
 #include <opencv2/opencv.hpp>
-
-#ifdef USING_GUI
-// 将MAT转换成imgui需要的texture
-void Mat2Texture(cv::Mat &image, GLuint &imageTexture);
-// 异常
-static void glfw_error_callback(int error, const char *description)
-{
-    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
-}
-#endif
 
 // Main code
 int main(int, char **)
 {
-
-#ifdef USING_GUI
-    // 用来产生你要操作的纹理对象的索引,放在外面
-    GLuint textures;
-    glGenTextures(1, &textures);
-    // 初始化
-    glfwSetErrorCallback(glfw_error_callback);
-    if (!glfwInit())
-        return 1;
-    // GL 3.0 + GLSL 130
-    const char *glsl_version = "#version 130";
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
-    // Create window with graphics context
-    GLFWwindow *window = glfwCreateWindow(1920, 1080, "606Vision", nullptr, nullptr);
-    if (window == nullptr)
-        return 1;
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // Enable vsync
-    // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO &io = ImGui::GetIO();
-    (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
-    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;     // Enable Docking
-    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;   // Enable Multi-Viewport / Platform Windows
-    // io.ConfigViewportsNoAutoMerge = true;
-    // io.ConfigViewportsNoTaskBarIcon = true;
-    // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-    // ImGui::StyleColorsLight();
-    // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular
-    // ones.
-    ImGuiStyle &style = ImGui::GetStyle();
-    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-    {
-        style.WindowRounding = 0.0f;
-        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
-    }
-    // Setup Platform/Renderer backends
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init(glsl_version);
-    // Our state
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-#endif
-
+    // 初始化相机
     mindvision::MVCamera *mv_capture_ = new mindvision::MVCamera(
         mindvision::CameraParam(0, mindvision::RESOLUTION_1280_X_1024, mindvision::EXPOSURE_5000));
     cv::Mat src_img_;
@@ -94,87 +30,8 @@ int main(int, char **)
         src_img_ = mv_capture_->image();
         // do something
 
-#ifdef USING_GUI
-        if (!glfwWindowShouldClose(window))
-        {
-            glfwPollEvents();
-
-            // Start the Dear ImGui frame
-            ImGui_ImplOpenGL3_NewFrame();
-            ImGui_ImplGlfw_NewFrame();
-            ImGui::NewFrame();
-
-            ImGui::Begin("Camera"); // Pass a pointer to our bool variable (the window will have a closing
-            ImGui::Text("frame (%.1f FPS)", io.Framerate);
-            Mat2Texture(src_img_, textures);
-            ImGui::Image(&textures, ImGui::GetContentRegionAvail());
-            ImGui::End();
-
-            // Rendering
-            ImGui::Render();
-            int display_w, display_h;
-            glfwGetFramebufferSize(window, &display_w, &display_h);
-            glViewport(0, 0, display_w, display_h);
-            glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w,
-                         clear_color.w);
-            glClear(GL_COLOR_BUFFER_BIT);
-            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-            // Update and Render additional Platform Windows
-            // (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to
-            // paste this code elsewhere.
-            //  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
-            if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-            {
-                GLFWwindow *backup_current_context = glfwGetCurrentContext();
-                ImGui::UpdatePlatformWindows();
-                ImGui::RenderPlatformWindowsDefault();
-                glfwMakeContextCurrent(backup_current_context);
-            }
-
-            glfwSwapBuffers(window);
-        }
-        else
-        {
-            break;
-        }
-#endif
         mv_capture_->releaseBuff();
     }
 
-#ifdef USING_GUI
-    // Cleanup
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
-#endif
-
     return 0;
 }
-
-#ifdef USING_GUI
-void Mat2Texture(cv::Mat &image, GLuint &imageTexture)
-{
-    if (image.empty())
-    {
-        std::cout << "image is empty! " << std::endl;
-        return;
-    }
-    else
-    {
-        cv::Mat bgra_image;
-        cv::cvtColor(image, bgra_image, cv::COLOR_BGR2BGRA);
-        // generate texture using GL commands
-        glBindTexture(GL_TEXTURE_2D, imageTexture);
-
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_BGRA, bgra_image.cols, bgra_image.rows, 0, GL_RGBA, GL_UNSIGNED_BYTE,
-                     bgra_image.data);
-    }
-}
-#endif
